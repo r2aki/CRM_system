e2fb61e8da1060e7bc2216f2ade8aee1e1b3bbcd
Ivan <sir239@yandex.ru>
2025-11-04 12:53:31 +0300

    Registration app

--- crm_project/registration/views.py
+++ crm_project/registration/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.messages.views import SuccessMessageMixin
+from users.forms import CustomUserCreationForm
 
-# Create your views here.
+
+class RegistrationView(SuccessMessageMixin, CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/registration_form.html'
+    success_url = reverse_lazy('dashboard')
+    success_message = "Регистрация успешно завершена! Добро пожаловать в систему."
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
